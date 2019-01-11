8926ca6c338ac7ace7f7978e719f47865311beb6
Mpho Mphego <mpho112@gmail.com>
2019-01-11 12:33:37 +0200

    Updated installer.sh

--- installer.sh
+++ installer.sh
@@ -90,17 +90,17 @@ InstallThis wget curl gdebi
 function ReposInstaller {
     cecho "${green}" "Adding APT Repositories."
     Version=$(lsb_release -cs)
-    add-apt-repository -y ppa:git-core/ppa
+    sudo add-apt-repository -y ppa:git-core/ppa
 
     wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
     echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-    [ -z "${Version}" ] || add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu ${Version} stable"
+    [ -z "${Version}" ] || sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu ${Version} stable"
 
     curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
     sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
-    sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+    sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 
     sudo add-apt-repository -y ppa:maarten-baert/simplescreenrecorder
     sudo add-apt-repository -y ppa:openshot.developers/ppa
