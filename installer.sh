6c5bc953c42b0624a6e7a83a00ccc21a6e650a03
Mpho Mphego <mmphego@ska.ac.za>
2019-01-12 04:24:56 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -8,12 +8,12 @@
 set -e
 
 # Set the colours you can use
-black=$(tput setaf 0)
+# black=$(tput setaf 0)
 red=$(tput setaf 1)
 green=$(tput setaf 2)
 yellow=$(tput setaf 3)
 blue=$(tput setaf 4)
-magenta=$(tput setaf 5)
+# magenta=$(tput setaf 5)
 cyan=$(tput setaf 6)
 white=$(tput setaf 7)
 # Resets the style
