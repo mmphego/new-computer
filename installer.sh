66e6b1f5b396f4e269c957e9c2a712191fb376cc
Mpho Mphego <mpho112@gmail.com>
2019-02-05 14:36:01 +0200

    Issues with unsigned repos

--- installer.sh
+++ installer.sh
@@ -106,8 +106,7 @@ function ReposInstaller {
 
     ## Sublime Text
     wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add - || true
-    echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list || true
-
+    echo "deb [trusted=yes] https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list || true
     ## Docker
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - || true
     [ -z "${Version}" ] || sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu ${Version} stable"
