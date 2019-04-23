ac5277eb75800a624d9cf6b4895dbe3fc7231d7e
Mpho Mphego <mpho112@gmail.com>
2019-04-23 11:56:02 +0200

    Updated devbox.sh
    Signed-off-by: Mpho Mphego <mpho112@gmail.com>

--- devbox.sh
+++ devbox.sh
@@ -19,10 +19,32 @@ function install_dot_files {
     wget -O .bash_aliaseshttps://raw.githubusercontent.com/mmphego/dot-files/master/.dotfiles/.bash_aliases
     wget -O .gitconfighttps://raw.githubusercontent.com/mmphego/dot-files/master/.dotfiles/.gitconfig
     wget -O .bashhttps://raw.githubusercontent.com/mmphego/dot-files/master/.dotfiles/.git-completion.bash
-    wget -O .nanorchttps://raw.githubusercontent.com/mmphego/dot-files/master/.dotfiles/.nanorc
     wget -O .profilehttps://raw.githubusercontent.com/mmphego/dot-files/master/.dotfiles/.profile
 }
 
+function update_packages {
+    sudo add-apt-repository -y ppa:git-core/ppa
+    sudo apt-get update
+    sudo apt-get install -y build-essential \
+                            colordiff \
+                            curl \
+                            git \
+                            gnupg2 \
+                            htop \
+                            nano \
+                            python3.6 \
+                            python3.6-dev \
+                            vim
+    URL=$(curl -s "https://api.github.com/repos/github/hub/releases/latest" | $(command -v grep) "browser_" | cut -d\" -f4 | $(command -v grep) "linux-amd64") || true
+    wget "${URL}" -O - | tar -zxf - || true
+    echo "Installing Hub"
+    find . -name "hub*" -type d | while read -r DIR;do
+        sudo prefix=/usr/local "${DIR}"/install
+        rm -rf -- hub-linux* || true
+    done
 
+}
+
+update_packages
 install_dot_files
 install_git_hooks
